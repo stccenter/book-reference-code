6197c323820ee612b15ca3f5ecda8432bbcf4d4f
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:50:00 -0400

    Update Code 9.13.py

--- 9/Code 9.13.py
+++ 9/Code 9.13.py
@@ -1,9 +1,10 @@
+#Please change this file path to your data location
 data = "O:\\Book\\Code\\9\\chp9Data\\bookSampleData.gdb\\railway"
 dscb = arcpy.Describe(data)
 
 if dscb.shapeType == "Polygon":
-	print "I am polygon"
+	print("I am polygon")
 elif dscb.shapeType == "Polyline":
-	print "I am polyline"
+	print("I am polyline")
 else:
-	print "I am not either polyline or polygon"
+	print("I am not either polyline or polygon")
