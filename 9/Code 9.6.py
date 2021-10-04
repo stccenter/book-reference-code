6d6a707e4e0455e402a20992d320c8b47b8b5358
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:43:33 -0400

    Update Code 9.6.py

--- 9/Code 9.6.py
+++ 9/Code 9.6.py
@@ -1,4 +1,4 @@
 with arcpy.da.SearchCursor(inputdata, ["SHAPE@", "SHAPE@LENGTH"], "FID < 10") as rows:
 	for row in rows:
 		# return the x and y of the first point and the length of each feature
-		print "{}, {}\n".format(row[0].firstPoint, row[1])
+		print("{}, {}\n".format(row[0].firstPoint, row[1]))
