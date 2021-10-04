2f71e535fb06ca76f2e50eb4bbc403492252b0a0
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:55:02 -0400

    Update Code 12.1.py

--- 12/Code 12.1.py
+++ 12/Code 12.1.py
@@ -1,3 +1,4 @@
+#Please change this file path to your data location
 arcpy.env.workspace = r"C:\\ArcGISdata\\chp12data.gdb"
 
 """
@@ -5,6 +6,7 @@ arcpy.env.workspace = r"C:\\ArcGISdata\\chp12data.gdb"
 	The TIN data cannot be saved in a geodatabase, so the output data
 	should be put into a folder e.g. C:\\ArcGISdata\\chp12data.gdb\\tin
 """
+#Please change this file path to your data location
 arcpy.RasterTin_3d("dem", r"E:\\ArcGISdata\\chp12data.gdb\\tin") 
 
 """
@@ -13,6 +15,7 @@ arcpy.RasterTin_3d("dem", r"E:\\ArcGISdata\\chp12data.gdb\\tin")
 	float type, and the methos used to raster the DEM is linear
 	interpolation 
 """
+#Please change this file path to your data location
 arcpy.TinRaster_3d(in_tin=r"E:\\ArcGISdata\\chp12data.gdb\\tin", out_raster="demFromTIN", \
                    data_type="FLOAT", method="LINEAR", \
                    sample_distance="CELLSIZE 50",z_factor="1")
