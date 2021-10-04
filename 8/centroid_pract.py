e11cd8bd2a5317fc2c80a07d08e4bbb7c57af61a
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:01:37 -0400

    Update centroid_pract.py

--- 8/centroid_pract.py
+++ 8/centroid_pract.py
@@ -23,9 +23,9 @@ class Polygon:
         ## ybar = sum of ((y[i+1]+y[i])*(x[i+1]*y[i]-x[i]*y[i+1]))/6*area
         """
         area = self.getArea()
-        print "Area is: ", area
+        print("Area is: ", area)
         while index < len(self.points)-1:
-            print self.points[index+1].x 
+            print(self.points[index+1].x)
             curentXBar = (1.0/(6.0*area))*(self.points[index+1].x + self.points[index].x)\
                          *(self.points[index+1].x * self.points[index].y - \
                             self.points[index].x * self.points[index+1].y)
@@ -35,7 +35,7 @@ class Polygon:
                             self.points[index].x * self.points[index+1].y)
             yBar = yBar + curentYBar
             index += 1
-        print 'xBar, yBar', xBar, yBar
+        print('xBar, yBar', xBar, yBar)
         centroid = Point(xBar, yBar) 
         return centroid       
         
@@ -47,7 +47,7 @@ class Polygon:
         ## area = sum of ((x[i+1]*y[i]-x[i]*y[i+1]))/2        
         """
         while index < len(self.points)-1:
-            print 'points: ', self.points[index].x, self.points[index].y
+            print('points: ', self.points[index].x, self.points[index].y)
             
             currentArea = (1.0/2.0)*(self.points[index+1].x * self.points[index].y - \
                           self.points[index].x * self.points[index+1].y)
@@ -78,7 +78,7 @@ def main():
     def clickHandler(x,y): 
         global startDraw
         global points
-        print "startDraw: " , startDraw, x, y
+        print("startDraw: " , startDraw, x, y)
         if startDraw:
             t.setposition(x, y)         
         else:            
@@ -105,7 +105,7 @@ def main():
         ## handle the polygon, snap last point and make sure it is the same as the first point
         global points
         num = len(points)
-        print 'points number: ', num
+        print('points number: ', num)
         """
         Since the last point should be the same as the first point, we need
         to delete the last point first, and then append the first point
@@ -113,7 +113,7 @@ def main():
         del points[num-1] 
         points.append(Point(points[0].x,points[0].y))
         for point in points: 
-            print point.x, point.y
+            print(point.x, point.y)
             t.setposition(point.x, point.y)
             t.dot(20, "blue") ## Create a circle on each vertice of the polygon
             t.write(str(point.x)+','+str(point.y)) ## Label the x ,y value on each vertice
@@ -125,7 +125,7 @@ def main():
         global startDraw
         ## Reset the global variable startDraw
         startDraw = False
-        print 'Clean all features'
+        print('Clean all features')
         t.reset()   ## Reset turtle
     
     cleanDraw = Tkinter.Button(frame, width = 15, text= 'Clean Draw',fg="blue", command=cleanHandler)
@@ -133,7 +133,7 @@ def main():
     def getCentroidHandler():
         polygon = Polygon(points)
         centroid = polygon.getCentroid()
-        print 'Centroid is : ', centroid.x, centroid.y
+        print('Centroid is : ', centroid.x, centroid.y)
         ## Now label the centroid
         t.penup()    
         t.setposition(centroid.x, centroid.y)
@@ -145,7 +145,7 @@ def main():
     calcuateCentroid.pack()
         ## declare botton event handler and a button
     def quitHandler():
-        print 'GoodBye'
+        print('GoodBye')
         #sys.exit(1)
         os._exit(1)
     button = Tkinter.Button(frame,width = 15, text= 'Quit',fg="blue", command=quitHandler)
