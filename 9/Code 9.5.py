5a89cc92063ef6dde9536555d59fe1b1e94fa002
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:43:49 -0400

    Update Code 9.5.py

--- 9/Code 9.5.py
+++ 9/Code 9.5.py
@@ -1,6 +1,7 @@
+#Please change this file path to your data location
 inputdata = "O:\\Book\\Code\\9\\chp9Data\\Partial_Streets.shp"
 
 # select the features with FID < 10
 with arcpy.da.SearchCursor(inputdata, ["Shape_Leng", "NAME", "TYPE"], "FID < 10") as rows:
 	for row in rows:
-		print "{}, {}, {}\n".format(row[0], row[1], row[2])
+		print("{}, {}, {}\n".format(row[0], row[1], row[2]))
