aba5c1116520e6d4cd70b8f7e443604830cbf086
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:49:24 -0400

    Update Code 9.14.py

--- 9/Code 9.14.py
+++ 9/Code 9.14.py
@@ -1,9 +1,10 @@
 import arcpy
 
 # list the field of the data "roads.shp" under the folder "ArcGISdata"
+#Please change this file path to your data location
 fieldlists = arcpy.ListFields("O:\\Book\\Code\\9\\chp9Data\\bookSampleData.gdb\\roads")
 
 # observe each field in the returning list
 for field in fieldlists:
 	# print out the property of the field, its name, scale, and type
-	print field.name,field.scale,field.type
+	print(field.name,field.scale,field.type)
