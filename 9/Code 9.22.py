7a0b58d26bcb352a0050dedcaae89384ad6d84b1
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:46:02 -0400

    Update Code 9.22.py

--- 9/Code 9.22.py
+++ 9/Code 9.22.py
@@ -1,9 +1,10 @@
 import arcpy
-
+#Please change this file path to your data location
 arcpy.env.workspace = ("O:\\Book\\Code\\9\\chp9Data")
 desc = arcpy.Describe('school.shp')
 spatialRef = desc.SpatialReference
 
 # create the FDS using the describe object's SR(SpatialReference) object
+#Please change this file path to your data location
 arcpy.CreateFeatureDataset_management('O:\\Book\\Code\\9\\chp9Data\\Default.gdb', 'results', spatialRef)
 
