c142d00ef9e5ab7db3833d404c1980ee6c1df9f6
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:54:17 -0400

    Update Code 12.4.py

--- 12/Code 12.4.py
+++ 12/Code 12.4.py
@@ -1,4 +1,5 @@
 # set the workspace
+#Please change this file path to your data location
 arcpy.env.workspace = r'C:\\ArcGISdata\\chp12data.gdb'
 aspectly = arcpy.sa.Aspect("dem")
 aspectly.save("aspect")
