88a1155cbe717c2f48007f14f1162237b57da585
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:48:21 -0400

    Update Code 9.17.py

--- 9/Code 9.17.py
+++ 9/Code 9.17.py
@@ -1,5 +1,5 @@
 import arcpy
-
+#Please change this file path to your data location
 fc = "O:\\Book\\Code\\9\\chp9Data\\bookSampleData.gdb\\roads"
 desc = arcpy.Describe(fc)
 # get a list of field objects from the describe object
@@ -7,11 +7,11 @@ fields = desc.fields
 
 for field in fields:
 	# manipulate field object and print out property of each field
-	print field.name
-	print field.aliasName
-	print field.type
+	print(field.name)
+	print(field.aliasName)
+	print(field.type)
 	if field.type == "Double":
-		print field.scale
+		print(field.scale)
 
 
 		
