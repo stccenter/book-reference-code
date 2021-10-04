9e4c282d258097bdc666f4500f9a55edaeff338a
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:46:20 -0400

    Update Code 9.21.py

--- 9/Code 9.21.py
+++ 9/Code 9.21.py
@@ -1,8 +1,9 @@
 import arcpy
-
+#Please change this file path to your data location
 arcpy.env.workspace = ("O:\\Book\\Code\\9\\chp9Data")
 # use the name of the coordinate system
 spatialRef = arcpy.SpatialReference("Hawaii Albers Equal Area Conic")
 # create the FDS using the spatialRef created from arcpy.SpatialReference() method
+#Please change this file path to your data location
 arcpy.CreateFeatureDataset_management('O:\\Book\\Code\\9\\chp9Data\\Default.gdb', 'results', spatialRef)
 
