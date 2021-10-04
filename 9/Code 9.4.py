8eb147f2a1c2f79cd9b42529bfef99c8aff0a693
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:44:08 -0400

    Update Code 9.4.py

--- 9/Code 9.4.py
+++ 9/Code 9.4.py
@@ -1,4 +1,5 @@
+#Please change this file path to your data location
 inputdata = "O:\\Book\\Code\\9\\chp9Data\\Partial_Streets.shp"
 with arcpy.da.SearchCursor(inputdata, ["Shape_Leng", "NAME", "TYPE"]) as rows:
 	for row in rows:
-		print "{}, {}, {}\n".format(row[0], row[1], row[2])
+		print("{}, {}, {}\n".format(row[0], row[1], row[2]))
