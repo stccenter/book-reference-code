36ed6995bb64e58f147ee2b2b9ed9658d229c2ea
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-04 19:54:27 -0400

    Update Code 12.3.py

--- 12/Code 12.3.py
+++ 12/Code 12.3.py
@@ -1,4 +1,5 @@
 # set the workspace
+#Please change this file path to your data location
 arcpy.env.workspace = r'C:\\ArcGISdata\\chp12data.gdb'
 
 # the input is DEM, and slope is in the unit of degrees
